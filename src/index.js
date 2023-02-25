f7badf03cf204e9e61bafd0fbda4d719572e9b28
Ignacio <iggyhuguet1024@gmail.com>
2023-02-25 16:07:38 -0600

    tasklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+
+// });
+
+const input = document.getElementById("new-task-description");
+
+const lists = document.getElementById("list")
+
+const form = document.getElementById("create-task-form")
+
+const tasks = document.getElementById("tasks")
+
+const deleteButton = document.createElement("button")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  let listItem = document.createElement('li');
+  listItem.appendChild(document.createTextNode(input.value));
+  document.querySelector('ul').appendChild(listItem);
+  document.querySelector('li').appendChild(deleteButton);
+  form.reset()
+})
+
+
+
+  // deleteButton.className = "delete-bttn";
+  // deleteButton.textContent = "X";
+  // deleteButton.addEventListener("click", () => {
+  // tasks.remove()
+// })
